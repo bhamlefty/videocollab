b348a3bdfefeca11ffe0b5b718ccd32db8626a45
cabake <CaseyBaker@Caseys-Mac-Pro.local>
2019-08-02 13:13:26 -0700

    Updates

---
+++ precache-manifest.cb7938c7bc8f7fedbd9ba9c6b0fb906e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e160a27f1e2267ea0c5b25932f87cb",
+    "url": "/videocollab/index.html"
+  },
+  {
+    "revision": "39a6a7f49dfcd3fe18c8",
+    "url": "/videocollab/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "32e0653e489d3e967975",
+    "url": "/videocollab/static/js/2.97eb1cff.chunk.js"
+  },
+  {
+    "revision": "39a6a7f49dfcd3fe18c8",
+    "url": "/videocollab/static/js/main.2a90949c.chunk.js"
+  },
+  {
+    "revision": "748f8ad2efd60d983116",
+    "url": "/videocollab/static/js/runtime~main.113361f0.js"
+  },
+  {
+    "revision": "b220a35d8a465c54ab4daa59e80d3615",
+    "url": "/videocollab/static/media/video.b220a35d.mp4"
+  }
+]);
