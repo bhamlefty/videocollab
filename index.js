e82cb1e512a69712e5f49ae218e959fd38b06add
Alyssa Baker <alyssabaker@Alyssas-MacBook-Pro.local>
2019-08-05 17:30:30 -0700

    New name

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res) {
 
 const port = 8000;
 const PORT = process.env.PORT || 5000;
-server.listen(PORT);
+app.listen(PORT);
 console.log('listening on port ', PORT);
 
 io.on('connection', (client) => {
